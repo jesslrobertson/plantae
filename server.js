b82e421590b43f79124f1a53cc5f429254d1524c
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-10-21 17:45:14 -0600

    cors config

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const bodyParser = require("body-parser");
 app.use(express.json());
 app.use(morgan("dev"));
 app.disable("etag");
-app.use(cors());
+app.use(cors({
+  origin: true
+}))
 app.use(
   fileUpload({
     createParentPath: true,
